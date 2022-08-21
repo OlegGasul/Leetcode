f1db46f07419d0b2bd2d2e02e6f6096559c0c296
OlegGasul <oleg.gasul@gmail.com>
2022-08-22 02:55:59 +0300

    The k Strongest Values in an Array

--- the-k-strongest-values-in-an-array.py
+++ the-k-strongest-values-in-an-array.py
@@ -2,15 +2,13 @@ import functools
 
 def getStrongest(arr, k: int):
     length = len(arr)
-    sortedArr = sorted(arr)
-    
-    median = sortedArr[(length - 1) // 2]
+    median = sorted(arr)[(length - 1) // 2]
     
     dp = [0] * length
 
     for i in range(length):
         dp[i] = [abs(arr[i] - median), arr[i]]
-    
+
     def compare(a, b):
         if a[0] == b[0]:
             return b[1] - a[1]
