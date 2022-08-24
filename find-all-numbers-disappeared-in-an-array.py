b792f99015d4044c091add09b92a116eec42e911
OlegGasul <oleg.gasul@gmail.com>
2022-08-24 23:51:03 +0300

    Find All Numbers Disappeared in an Array

--- find-all-numbers-disappeared-in-an-array.py
+++ find-all-numbers-disappeared-in-an-array.py
@@ -1,13 +1,15 @@
+# https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
 def findDisappearedNumbers(nums):
     length = len(nums)
     i = 0
     result = []
 
     while i < length:
-        correct = nums[i] - 1
-        while nums[i] != nums[correct]:
-            nums[i], nums[correct] = nums[correct], nums[i]
-            correct = nums[i] - 1
+        correctPos = nums[i] - 1
+        while 1 <= nums[i] <= length and nums[i] != nums[correctPos]:
+            nums[i], nums[correctPos] = nums[correctPos], nums[i]
+            correctPos = nums[i] - 1
         
         i += 1
 
@@ -18,5 +20,10 @@ def findDisappearedNumbers(nums):
     return result
     
 
-print(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
-print(findDisappearedNumbers([1, 1]))
+# print(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
+# print(findDisappearedNumbers([1, 4, 25, 10, 25]))
+# print(findDisappearedNumbers([1, 1, 2]))
+print(findDisappearedNumbers([5, 10]))
+
+# [4, 3, 2, 7, 8, 2, 3, 1]
+# [3, 2, 3, 4, 8, 2, 7, 1]
