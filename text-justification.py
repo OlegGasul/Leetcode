3ef7e8f04196f98b116469f81e26fb2410c247e7
OlegGasul <oleg.gasul@gmail.com>
2022-08-25 00:22:38 +0300

    Text Justification

---
+++ text-justification.py
@@ -0,0 +1,39 @@
+def fullJustify(words, maxWidth: int):
+    result = []
+
+    while words:
+        temp = []
+
+        length = maxWidth
+        space = False
+        while words and len(words[0]) + (1 if space else 0) < length:
+            word = words.pop(0)
+            
+            length -= len(word) + (1 if space else 0)
+            temp.append(word)
+            space = True
+
+        result.append(temp)
+
+    lines = []
+
+    while result:
+        isLastLine = len(result) == 1
+
+        line = result.pop(0)
+
+        count = 0
+        for i in range(len(line)):
+            count += len(line[i])
+
+        if len(line) > 1:
+            spaces = ' ' * ((maxWidth - count) // (len(line) - 1))
+            lines.append(spaces.join(line))
+        else:
+            lines.append(line.pop().ljust(maxWidth, ' '))
+
+    return lines
+
+
+
+print(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16))
