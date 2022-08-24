3059995870282cd90bad3fa13aef76bcc96e2cbe
OlegGasul <oleg.gasul@gmail.com>
2022-08-25 00:25:36 +0300

    Text Justification

--- text-justification.py
+++ text-justification.py
@@ -37,3 +37,4 @@ def fullJustify(words, maxWidth: int):
 
 
 print(fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16))
+print(fullJustify(["Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"], 20))
