c58e00bff7f74589d971c21fe27ef4ffc5672c9e
OlegGasul <oleg.gasul@gmail.com>
2022-08-24 07:29:59 +0300

    Intersection of Multiple Arrays

---
+++ intersection-of-multiple-arrays.py
@@ -0,0 +1,18 @@
+import collections
+
+def intersection(nums):
+    length = len(nums)
+    counter = collections.Counter()
+
+    for i in range(length):
+        for j in range(len(nums[i])):
+            counter[nums[i][j]] += 1
+
+    result = []
+    for num, count in counter.items():
+        if count == length:
+            result.append(num)
+
+    return result
+
+print(intersection([[3, 1, 2, 4, 5], [1, 2, 3, 4], [3, 4, 5, 6]]))
