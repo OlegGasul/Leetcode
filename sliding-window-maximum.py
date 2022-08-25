4f2c32bd933d9e7fffb859037988cdaac1af9af3
OlegGasul <oleg.gasul@gmail.com>
2022-08-25 10:36:58 +0300

    Sliding Window Maximum

--- sliding-window-maximum.py
+++ sliding-window-maximum.py
@@ -1,10 +1,21 @@
 from collections import deque
 
 def maxSlidingWindow(nums, k: int):
+    if k == 0:
+        return []
+
+    if k == 1:
+        return nums
+
+    length = len(nums)
+
+    if k == length:
+        return [max(nums)]
+    
     result = []
     queue = deque()
 
-    for i in range(len(nums)):
+    for i in range(length):
         num = nums[i]
             
         if queue and queue[0] <= i - k:
@@ -22,3 +33,4 @@ def maxSlidingWindow(nums, k: int):
 
 print(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
 print(maxSlidingWindow([1, 2, 3, 4, 5, 6], 3))
+print(maxSlidingWindow([1, 2, 3, 4, 5, 6], 6))
