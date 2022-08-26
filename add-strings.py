f56779dcdee05896b81790ad2721df9c21f03915
OlegGasul <oleg.gasul@gmail.com>
2022-08-26 13:19:18 +0300

    Add Strings

---
+++ add-strings.py
@@ -0,0 +1,26 @@
+def addStrings(num1: str, num2: str) -> str:
+    num1 = list(num1)
+    num2 = list(num2)
+
+    result = []
+    rest = 0
+
+    while num1 or num2 or rest:
+        value = (int(num1.pop()) if num1 else 0) + (int(num2.pop()) if num2 else 0) + rest
+
+        result.insert(0, str(value % 10))
+        rest = value // 10
+
+    while num1 or rest:
+        value = (int(num1.pop()) if num1 else 0) + rest
+        result.insert(0, str(value % 10))
+        rest = value // 10
+
+    return ''.join(result)
+    
+
+print(addStrings("11", "123"))
+print(addStrings("123", "11"))
+print(addStrings("123", "17"))
+print(addStrings("999", "1"))
+print(addStrings("999", "99999"))
