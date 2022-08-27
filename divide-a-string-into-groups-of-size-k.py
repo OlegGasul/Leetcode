dac3e15ec9f9d094945110e91d87ef595cb2a6e6
OlegGasul <oleg.gasul@gmail.com>
2022-08-27 18:07:10 +0300

    Divide a String Into Groups of Size k

---
+++ divide-a-string-into-groups-of-size-k.py
@@ -0,0 +1,16 @@
+def divideString(s: str, k: int, fill: str):
+    length = len(s)
+        
+    result = []
+        
+    i = 0
+    while i < length:
+        result.append(s[i : i + k])
+        i += k
+    
+    if len(result[-1]) < k:
+        result[-1] = result[-1].ljust(k, fill)
+        
+    return result
+
+print(divideString("ctoyjrwtngqwt", 8, "n"))
