f17ec3d75858c06cbc7389547dd57ba3f61d79de
OlegGasul <oleg.gasul@gmail.com>
2022-08-27 19:59:55 +0300

    Valid Palindrome

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -1,11 +1,28 @@
 def isPalindrome(s: str) -> bool:
-    alnum = ""
-    for letter in s:
-        if letter.isalnum():
-            alnum += letter.lower()
-    return alnum == alnum[::-1]
+    left = 0
+    right = len(s) - 1
+    
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        
+        while right > left and not s[right].isalnum():
+            right -= 1
+
+        if left == right:
+            break
+
+        if s[left].lower() != s[right].lower():
+            return False
+        
+        left += 1
+        right -= 1
+
+    return True
 
 print(isPalindrome("A man, a plan, a canal: Panama"))
 print(isPalindrome("race a car"))
 print(isPalindrome("0P"))
 print(isPalindrome(" "))
+print(isPalindrome("race a car"))
+print(isPalindrome(".,"))
