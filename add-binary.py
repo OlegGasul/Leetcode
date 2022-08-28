8c8e5f4bd311b71cf407b1d94f62913da3423689
OlegGasul <oleg.gasul@gmail.com>
2022-08-28 08:10:48 +0300

    Add Binary

---
+++ add-binary.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+def addBinarySimple(a: str, b: str) -> str:
+    return str(int(a, 2) + int(b, 2))[2:]
+
+def addBinary(a: str, b: str) -> str:
+    a = list(a)
+    b = list(b)
+
+    carry = 0
+    result = []
+
+    while a or b or carry == 1:
+        bit1 = int(a.pop()) if a else 0
+        bit2 = int(b.pop()) if b else 0
+        
+        value = bit1 ^ bit2 ^ carry
+        carry = 1 if bit1 + bit2 + carry >= 2 else 0
+        
+        result.insert(0, value)
+
+    return ''.join([str(x) for x in result])
+
+
+print(addBinary("11", "1"))
+print(addBinary("1111", "1111"))
