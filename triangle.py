1fac5e08a076e9b0f90b4439c8636a4b9e235661
OlegGasul <oleg.gasul@gmail.com>
2022-08-29 19:57:40 +0300

    Triangle

--- triangle.py
+++ triangle.py
@@ -1,4 +1,4 @@
-def minimumTotal(triangle) -> int:
+def minimumTotal2(triangle) -> int:
     def minimum(i, j):
         if len(triangle) - 1 == i:
             return min(triangle[i][j], triangle[i][j])
@@ -6,6 +6,21 @@ def minimumTotal(triangle) -> int:
 
     return minimum(0, 0)
 
+def minimumTotal(triangle) -> int:
+    for i in range(1, len(triangle)):        
+        for j in range(i + 1):
+            temp1, temp2 = float('inf'), float('inf')
+            
+            if j - 1 >= 0:
+                temp1 = triangle[i - 1][j - 1]
+            
+            if j < i:
+                temp2 = triangle[i - 1][j]
+            
+            triangle[i][j] = min(temp1, temp2) + triangle[i][j]
+
+    return min(triangle[-1])
+        
 
 print(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]))
-print(minimumTotal([[-10]]))
+# print(minimumTotal([[-10]]))
