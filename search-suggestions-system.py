05b35b0a223bbc01506d552e792c8dab205a802d
OlegGasul <oleg.gasul@gmail.com>
2022-08-30 20:00:48 +0300

    Search Suggestions System

---
+++ search-suggestions-system.py
@@ -0,0 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.words = []
+        self.endOfString = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insertString(self, word):
+        current = self.root
+        for char in word:
+            node = current.children.get(char)
+            if node == None:
+                node = TrieNode()
+                current.children.update({ char: node })
+            current = node
+            current.words.append(word)
+            current.words.sort()
+        
+        current.endOfString = True
+
+    def searchString(self, word, maxSize):
+        currentNode = self.root
+        for i in word:
+            node = currentNode.children.get(i)
+            currentNode = node
+            if not currentNode:
+                break
+
+        return currentNode.words[0 : maxSize] if currentNode else []
+
+class Solution:
+    def suggestedProducts(self, products, searchWord: str):
+        trie = Trie()
+
+        for product in products:
+            trie.insertString(product)
+        
+        result = []
+        for i in range(len(searchWord)):
+            result.append(trie.searchString(searchWord[0 : i + 1], 3))
+
+        return result
+
+
+
+solution = Solution()
+print(solution.suggestedProducts(["mobile", "mouse", "moneypot", "monitor", "mousepad"], "mouse"))
+print(solution.suggestedProducts(["havanna"], "mouse"))
