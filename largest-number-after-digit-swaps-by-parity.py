a3a211c670723770f360479b3bde79663873421e
OlegGasul <oleg.gasul@gmail.com>
2022-08-31 19:18:47 +0300

    Largest Number After Digit Swaps by Parity

--- largest-number-after-digit-swaps-by-parity.py
+++ largest-number-after-digit-swaps-by-parity.py
@@ -2,31 +2,22 @@ import functools
 
 def largestInteger(num: int) -> int:
     nums = [int(x) for x in list(str(num))]
-        
+    
     length = len(nums)
     evens = []
-    evensIndexes = []
     odds = []
-    oddsIndexes = []
-
+    
     for i in range(length):
         if nums[i] % 2 == 0:
             evens.append(nums[i])
-            evensIndexes.append(i)
         else:
             odds.append(nums[i])
-            oddsIndexes.append(i)
 
-    evens.sort(reverse = True)
-    evensIndexes.sort()
-    odds.sort(reverse = True)
-    oddsIndexes.sort()
+    evens.sort()
+    odds.sort()
 
-    for i in range(len(evens)):
-        nums[evensIndexes[i]] = evens[i]
-
-    for i in range(len(odds)):
-        nums[oddsIndexes[i]] = odds[i]
+    for i in range(len(nums)):
+        nums[i] = evens.pop() if nums[i] % 2 == 0 else odds.pop()
 
     return int(''.join([str(x) for x in nums]))
 
