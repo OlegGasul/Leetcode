e2b46d7e754199e8e18190713cbd6de30d02abcc
OlegGasul <oleg.gasul@gmail.com>
2022-09-02 15:42:37 +0300

    Longest increasing subsequence

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -1,34 +1,14 @@
-def findIndexInLis(lis, value):
-    left = 0
-    right = len(lis) - 1
+def lengthOfLIS(nums) -> int:
+    dp = [1 for _ in range(len(nums))]
+    result = 1
+    dp[0] = 1
+        
+    for i in range(1, len(nums)):
+        for j in range(i):
+            if nums[i] >= nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+                result = max(dp[i], ret)
+    
+    return result
 
-    while left <= right:
-        index = left + (right - left) // 2
-        middle = lis[index]
-
-        if value == middle:
-            return index
-
-        if value > middle:
-            left = index + 1
-        else:
-            right = index - 1
-
-    return left
-
-def lengthOfLIS(nums, start, end) -> int:
-    lis = [nums[start]]
-
-    for i in range(start + 1, end):
-        if nums[i] > lis[-1]:
-            lis.append(nums[i])
-        else:
-            index = findIndexInLis(lis, nums[i])
-            lis[index] = nums[i]
-
-        print(lis)
-
-    return len(lis)
-
-
-print(lengthOfLIS([10, 100, 101, 102, 103, 104, 9, 2, 5, 3, 7, 8, 9, 10, 101, 18], 0, 3))
+print(lengthOfLIS([1, 2, 3, 4, 1, 1, 1, 2]))
