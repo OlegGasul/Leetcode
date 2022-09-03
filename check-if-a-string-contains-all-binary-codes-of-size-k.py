b25018d579c013753a9a5bbd1c0fff8b81316b0f
OlegGasul <oleg.gasul@gmail.com>
2022-09-03 10:58:21 +0300

    Check If a String Contains All Binary Codes of Size K

--- check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,10 +1,13 @@
 def hasAllCodes(s: str, k: int) -> bool:
     unique = set()
-        
+    total = 2 ** k
+
     for i in range(len(s) - k + 1):
         unique.add(s[i : i + k])
+        if len(unique) == total:
+            return True
             
-    return len(unique) == 2 ** k
+    return False
 
 print(hasAllCodes("00110110", 2))
 print(hasAllCodes("0110", 1))
