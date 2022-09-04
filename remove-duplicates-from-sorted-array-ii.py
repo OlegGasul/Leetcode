b8d29e7a0c7af5044ca98ba195c02155a2b78d6e
OlegGasul <oleg.gasul@gmail.com>
2022-09-04 05:45:57 +0300

    Remove Duplicates from Sorted Array II

--- remove-duplicates-from-sorted-array-ii.py
+++ remove-duplicates-from-sorted-array-ii.py
@@ -1,6 +1,6 @@
 def removeDuplicates(nums) -> int:
     if len(nums) <= 2:
-        return nums
+        return len(nums)
     
     index = 2
     
@@ -17,5 +17,9 @@ print(removeDuplicates(nums))
 print(list(nums))
 
 nums = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 5]
+print(removeDuplicates(nums))
+print(list(nums))
+
+nums = [1]
 print(removeDuplicates(nums))
 print(list(nums))
