461e6fd5161f4a6d47bde42e6eaac602f2ba073a
OlegGasul <oleg.gasul@gmail.com>
2022-09-04 19:52:00 +0300

    Maximum Length of Subarray With Positive Product

--- maximum-length-of-subarray-with-positive-product-2.py
+++ maximum-length-of-subarray-with-positive-product-2.py
@@ -2,6 +2,7 @@ def getMaxLen(nums) -> int:
     length = len(nums)
     
     intervals = [[0, 0, []]]
+    allZeros = True
     
     for i in range(length):
         if nums[i] == 0:
@@ -10,12 +11,15 @@ def getMaxLen(nums) -> int:
             intervals[-1][1] = i
             if nums[i] < 0:
                 intervals[-1][2].append(i)
+            allZeros = False
+
+    if allZeros:
+        return 0
 
     def getMaxLenInterval(interval):
         if len(interval[2]) % 2 == 0:
             return interval[1] - interval[0] + 1
-
-        return max((interval[2][-1] - 1) - interval[0] + 1, interval[1] - (interval[2][0] + 1))
+        return max((interval[2][-1] - 1) - interval[0] + 1, interval[1] - (interval[2][0] + 1) + 1)
 
     maximum = float('-inf')
     for interval in intervals:
@@ -23,6 +27,8 @@ def getMaxLen(nums) -> int:
 
     return maximum
 
+print(getMaxLen([-1, 2]))
+print(getMaxLen([0, 0, 0, 0, 0]))
 print(getMaxLen([1, -2, -3, 4]))
 print(getMaxLen([0, 1, -2, -3, -4]))
 print(getMaxLen([-1, -2, -3, 0, 1]))
