ba9fcf44209a6c9a8d603d70d36eeaa5d4162911
OlegGasul <oleg.gasul@gmail.com>
2022-09-04 05:30:04 +0300

    Remove Duplicates from Sorted Array

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -1,22 +1,18 @@
 def removeDuplicates(nums) -> int:
-    prev = None
-    index = 0
+    index = 1
     
     while index < len(nums):
-        item = nums[index]
-
-        if item == prev:
+        if nums[index] == nums[index - 1]:
             nums.pop(index)
         else:
             index += 1
-        prev = item
 
     return index
 
-# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-# nums = [1, 1]
 nums = [1, 1, 2]
-
 print(removeDuplicates(nums))
+print(list(nums))
 
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+print(removeDuplicates(nums))
 print(list(nums))
