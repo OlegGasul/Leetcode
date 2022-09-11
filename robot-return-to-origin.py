67e8a18b330770db80900a86c3aae1d0dbc7681c
OlegGasul <oleg.gasul@gmail.com>
2022-09-11 21:42:37 +0300

    Robot Return to Origin

---
+++ robot-return-to-origin.py
@@ -0,0 +1,18 @@
+def judgeCircle(moves: str) -> bool:
+    i = 0
+    j = 0
+        
+    for move in moves:
+        if move == "U":
+            i -= 1
+        elif move == "R":
+            j += 1
+        elif move == "D":
+            i += 1
+        elif move == "L":
+            j -= 1
+                
+    return i == 0 and j == 0
+
+print(judgeCircle("UD"))
+print(judgeCircle("LL"))
