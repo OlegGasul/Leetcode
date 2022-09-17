1be2d1516941b9691117ca5052e073783ab659ca
OlegGasul <oleg.gasul@gmail.com>
2022-09-17 20:34:36 +0300

    Meeting Rooms III

---
+++ meeting-rooms-iii.py
@@ -0,0 +1,49 @@
+import heapq
+import functools
+from collections import Counter
+
+def mostBooked(n: int, meetings) -> int:
+    def compare(a, b):
+        return a[0] - b[0]
+    meetings = sorted(meetings, key = functools.cmp_to_key(compare))
+
+    counter = Counter()
+
+    rooms = [i for i in range(n)]
+    heapq.heapify(rooms)
+
+    meetingRooms = []
+    heapq.heapify(meetingRooms)
+
+    for meet in meetings:
+        # Release all rooms where end time is earlier than current start time
+        while meetingRooms and meetingRooms[0][0] <= meet[0]:
+            time, room = heapq.heappop(meetingRooms)
+            heapq.heapify(meetingRooms)
+
+            heapq.heappush(rooms, room)
+            heapq.heapify(rooms)
+        
+        if not rooms:
+            # If all rooms are busy at this moment
+            time, room = heapq.heappop(meetingRooms)
+            heapq.heappush(meetingRooms, [meet[1] + (time - meet[0]), room])
+            heapq.heapify(meetingRooms)
+
+            counter[room] += 1
+        else:
+            room = heapq.heappop(rooms)
+            heapq.heapify(rooms)
+
+            heapq.heappush(meetingRooms, [meet[1], room])
+            heapq.heapify(meetingRooms)
+
+            counter[room] += 1
+    
+    return counter.most_common()[0][0]
+
+
+print(mostBooked(4, [[19, 20], [14, 15], [13, 14], [11, 20]]))
+print(mostBooked(4, [[18, 19], [3, 12], [17, 19], [2, 13], [7, 10]]))
+print(mostBooked(2, [[0, 10], [1, 5], [2, 7], [3, 4]]))
+print(mostBooked(3, [[1, 20], [2, 10], [3, 5], [4, 9], [6, 8]]))
