a2f90623895362d13e8c989068d35c2dba859cce
OlegGasul <oleg.gasul@gmail.com>
2022-09-22 10:46:14 +0300

    Dijkstrta

--- dijkstra.py
+++ dijkstra.py
@@ -3,6 +3,7 @@ import heapq
 def dijkstra(n, graph, start):
     distances = [float('inf')] * n
     visited = [False] * n
+    prev = [None] * n
     distances[start] = 0
 
     pq = []
@@ -20,23 +21,30 @@ def dijkstra(n, graph, start):
 
                 newDistance = distances[index] + edge[1]
                 if newDistance < distances[edge[0]]:
+                    prev[edge[0]] = index
                     distances[edge[0]] = newDistance
                     heapq.heappush(pq, (newDistance, edge[0]))
                     heapq.heapify(pq)
 
-    return distances
+    return (distances, prev)
 
+def findShortestPath(n, graph, start, end):
+    dist, prev = dijkstra(n, graph, start)
+    path = []
+    if dist[end] == float('inf'):
+        return path
 
-print(dijkstra(5, {
+    node = end
+    while node:
+        path.insert(0, node)
+        node = prev[node]
+
+    path.insert(0, start)
+    return path
+
+print(findShortestPath(5, {
     0: {(1, 4), (2, 1)},
     2: {(1, 2), (3, 5)},
     1: {(3, 1)},
     3: {(4, 3)}
-}, 0))
-
-print(dijkstra(5, {
-    0: {(1, 4), (2, 1)},
-    2: {(1, 2), (3, 5)},
-    1: {(3, 1)},
-    3: {(4, 3)}
-}, 1))
+}, 0, 4))
