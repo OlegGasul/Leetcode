21cb5bd37cd127a9c0c77a8c5c67bbf499efebfe
OlegGasul <oleg.gasul@gmail.com>
2022-09-25 13:23:16 +0300

    Candy

--- candy.py
+++ candy.py
@@ -9,12 +9,14 @@ def candy(ratings) -> int:
     heapq.heapify(pq)
 
     candies = [0] * length
+    result = 0
 
     while pq:
         rating, i = heapq.heappop(pq)
         candies[i] = max(candies[i - 1] + 1 if i - 1 >= 0 and ratings[i - 1] < rating else 1, candies[i + 1] + 1 if i + 1 < length and ratings[i + 1] < rating else 1)
+        result += candies[i]
 
-    return sum(candies)
+    return result
 
 
 print(candy([1, 0, 2]))
