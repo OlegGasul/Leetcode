03e45b63bab3e2d9cae07ca0261be25c84671a97
OlegGasul <oleg.gasul@gmail.com>
2022-10-16 13:29:16 +0300

    Final Value of Variable After Performing Operations

---
+++ final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,10 @@
+def finalValueAfterOperations(operations) -> int:
+    result = 0
+        
+    for op in operations:
+        result += 1 if "+" in op else -1
+                
+    return result
+
+print(finalValueAfterOperations(["--X", "X++", "X++"]))
+print(finalValueAfterOperations(["++X", "++X", "X++"]))
