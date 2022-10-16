a6f8a074a9f8513a1d3050879aaca3ebff27e055
Oleg <oleg.gasul@gmail.com>
2022-10-16 18:15:07 +0300

    Update array-partition.py

--- array-partition.py
+++ array-partition.py
@@ -1,3 +1,5 @@
+# Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
 def arrayPairSum(nums) -> int:
     return sum(sorted(nums)[::2])
 
