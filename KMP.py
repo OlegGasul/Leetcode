882b784f87d607c647969bd4853c7b20836997ba
OlegGasul <oleg.gasul@gmail.com>
2022-10-25 12:12:05 +0300

    KMP

--- KMP.py
+++ KMP.py
@@ -1,37 +1,37 @@
 def isSubstring(s1, s2):
-    len1 = len(s1)
-    len2 = len(s2)
-
-    dp = [0] * len1
-    i = 0
-    j = 1
-    while j < len1:
-        if s1[i] == s1[j]:
-            i += 1
-            dp[j] = i
-            j += 1
-        elif i == 0:
-            dp[j] = 0
-            j += 1
-        else:
-            i = dp[i - 1]
-    
-    print(dp)
-
-    i = 0
-    j = 1
-
-    while i < len1 and j < len2:
-        if s1[i] == s2[j] and i == len1 - 1:
-            return True
-        elif s1[i] == s2[j]:
-            i += 1
-            j += 1
-        else:
-            i = dp[i - 1]
-            if i == 0:
+    def build_pattern(string):
+        pattern = [0 for s in string] 
+        j = 0
+        i = 1
+        
+        while i < len(string):
+            if string[i] == string[j]:
+                pattern[i] = j + 1
+                i += 1
                 j += 1
+            elif j > 0:
+                j = pattern[j-1]
+            else:
+                i += 1
+        return [-1] + pattern
 
-    return False
+    def does_match(string, substring, pattern):
+        i = 0
+        j = 0
+        
+        while i < len(string):
+            if string[i] == substring[j]:
+                if j == len(substring) - 1:
+                    return True
+                i += 1
+                j += 1
+            elif j > 0:
+                j = pattern[j]
+            else: 
+                i += 1
+        return False
 
-print(isSubstring("aaab", "aaanaaamaaab"))
+    return does_match(s2, s1, build_pattern(s1))
+
+# print(isSubstring("aaab", "aaanaaamaaab"))
+print(isSubstring("10", "0110"))
