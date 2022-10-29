f6da9cdc819070cbfb9c8f98ac090997eef47ad9
oleg <oleg.gasul@gmail.com>
2022-10-30 01:24:57 +0300

    Reduction Operations to Make the Array Elements Equal

--- reduction-operations-to-make-the-array-elements-equal.py
+++ reduction-operations-to-make-the-array-elements-equal.py
@@ -6,8 +6,6 @@ def reductionOperations(nums) -> int:
     for i in range(1, len(nums)):
         dp[i] = dp[i - 1] + (1 if nums[i] > nums[i - 1] else 0)
 
-    print(dp)
-        
     return sum(dp)
 
 print(reductionOperations([1, 1, 2, 2, 3]))
