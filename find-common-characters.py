ae6c0f4679be75e3212e41c20ef02d292af111bf
OlegGasul <oleg.gasul@gmail.com>
2022-10-29 12:49:32 +0300

    Find Common Characters

--- find-common-characters.py
+++ find-common-characters.py
@@ -1,18 +1,29 @@
+from collections import Counter
+
 def commonChars(words):
     length = len(words)
+    if length == 0:
+        return []
+        
     if length == 1:
         return list(words[0])
-    
-    words.sort(key = lambda s: len(s))
-    
-    chars = set(words[0])
-
+        
+    letters = Counter(words[0])
+        
     for i in range(1, length):
-        chars = chars.intersection(words[i])
-        if len(chars) == 0:
-            return []
-
-    return list(chars)
+        counter = Counter(words[i])
+            
+        for ch, count in letters.items():
+            letters[ch] = min(letters[ch], counter[ch])
+                
+    result = []
+    for ch, count in letters.items():
+        if count == 1:
+            result.append(ch)
+        else:
+            result.extend([ch] * count)
+                
+    return result
 
 
 print(commonChars(["bella", "label", "roller"]))
