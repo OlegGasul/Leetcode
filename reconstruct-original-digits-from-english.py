83d6d6aa54ed505429f7cd2f0f203f004c26466c
Oleg Gasul <oleg.gasul@gmail.com>
2022-11-08 19:06:40 +0100

    Reconstruct Original Digits from English

---
+++ reconstruct-original-digits-from-english.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+class Solution:
+    def originalDigits(self, s: str) -> str:
+        counter = Counter(s)
+        
+        letters = {
+            'w': ('two', '2'),
+            'u': ('four', '4'),
+            'x': ('six', '6'),
+            'f': ('five', '5'),
+            'z': ('zero', '0'),
+            'r': ('three', '3'),
+            't': ('eight', '8'),
+            's': ('seven', '7'),
+            'i': ('nine', '9'),
+            'n': ('one', '1')
+        }
+        
+        result = []
+        for ch, (word, digit) in letters.items():
+            digitCount = counter[ch]
+            result.append(digit * digitCount)
+		    
+            for c in word:
+                counter[c] -= digitCount
+	    
+        return ''.join(sorted(result))
+
+solution = Solution()
+print(solution.originalDigits("owoztneoer"))
+print(solution.originalDigits("fviefuro"))
