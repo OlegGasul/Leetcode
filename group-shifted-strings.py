b97f8f8b6266f972f7e8e3a8fe0f3fad16d15fd6
Oleg Gasul <oleg.gasul@gmail.com>
2022-11-29 23:03:44 +0200

    Group Shifted Strings

---
+++ group-shifted-strings.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+class Solution:
+    def groupStrings(self, strings):
+        groups = defaultdict(list)
+
+        for s in strings:
+            key = ""
+            for i in range(1, len(s)):
+                a = ord(s[i - 1])
+                b = ord(s[i])
+                
+                key += str((b - a + 26) % 26) + 'a'
+            
+            groups[key].append(s)
+        
+        result = []
+        for k, v in groups.items():
+            result.append(v)
+        
+        return result
+
+solution = Solution()
+print(solution.groupStrings(["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"]) == [["acef"], ["a", "z"], ["abc", "bcd", "xyz"], ["az", "ba"]])
+print(solution.groupStrings(["a"]) == [["a"]])
