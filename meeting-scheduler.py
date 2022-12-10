2784a236f2a3d5e5814caaf27d9fc14a143dcaa1
OlegGasul <oleg.gasul@gmail.com>
2022-12-10 20:20:45 +0200

    Meeting Scheduler

--- meeting-scheduler.py
+++ meeting-scheduler.py
@@ -1,16 +1,19 @@
 class Solution:
     def minAvailableDuration(self, slots1, slots2, duration: int):
-        slots1.sort(reverse = True)
-        slots2.sort(reverse = True)
+        slots1.sort()
+        slots2.sort()
+
+        i = 0
+        j = 0
         
-        while slots1 and slots2:
-            a, b = slots1[-1]
-            c, d = slots2[-1]
+        while i < len(slots1) and j < len(slots2):
+            a, b = slots1[i]
+            c, d = slots2[j]
 
             if c > b:
-                slots1.pop()
+                i += 1
             elif a > d:
-                slots2.pop()
+                j += 1
             else:
                 start = max(a, c)
                 end = min(b, d)
@@ -18,12 +21,12 @@ class Solution:
                     return [start, start + duration]
                 else:
                     if d > b:
-                        slots1.pop()
+                        i += 1
                     elif b > d:
-                        slots2.pop()
+                        j += 1
                     else:
-                        slots1.pop()
-                        slots2.pop()
+                        i += 1
+                        j += 1
         
         return []
 
